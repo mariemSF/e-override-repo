34cecd628855b9c506395c2b2db5fe6aeffc5be8
mohamed_klibi23 <mohamed_klibi23@HP-KLIBI>
2015-10-30 16:53:23 +0100

    Signed-off-by: mohamed_klibi23 <mohamed_klibi23@HP-KLIBI>

--- e-override-ejb/src/main/java/gestionCommandservices/impl/GestionCommandservice.java
+++ e-override-ejb/src/main/java/gestionCommandservices/impl/GestionCommandservice.java
@@ -1,8 +1,17 @@
 package gestionCommandservices.impl;
 
+import java.util.Date;
+import java.util.List;
+
+import entities.Command;
+import entities.Product;
 import gestionCommandservices.interfaces.GestionCommandserviceLocal;
 import gestionCommandservices.interfaces.GestionCommandserviceRemote;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  * Session Bean implementation class GestionCommandservice
@@ -10,11 +19,75 @@ import javax.ejb.Stateless;
 @Stateless
 public class GestionCommandservice implements GestionCommandserviceRemote, GestionCommandserviceLocal {
 
+	@PersistenceContext
+	private EntityManager entityManager;
     /**
      * Default constructor. 
      */
     public GestionCommandservice() {
         // TODO Auto-generated constructor stub
     }
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Command> findAllCommandsByClient(String ClientName) {
+		String jpql = "select c from Command c where c.client.name=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", ClientName);
+		return query.getResultList();
+	}
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Command> findAllCommandsByCategory(String ProductCategory) {
+		//List<Product> product = findAllProductsByCategory(product.);
+		//teams.add(team);
+		String jpql = "select c from Command c where c.product.category=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", ProductCategory);
+		return query.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Command> findAllCommandsByCommandDate(Date CommandDate) {
+		String jpql = "select c from Command c where c.dateCmd=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", CommandDate);
+		return query.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Command> findAllCommandsByDeliveryDate(Date DeliveryDate) {
+		String jpql = "select c from Command c where c.dateLivraison=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", DeliveryDate);
+		return query.getResultList();
+	}
+
+	
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Product> findAllProductsByIdCommand(Integer CommandId) {
+		Command command = entityManager.find(Command.class,
+				CommandId);
+		String jpql = "select p from Product p where p.command=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", command);
+		return query.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Product> findAllCommandsByClientName(String ClientName) {
+		String jpql = "select c from Command c where c.client.name=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", ClientName);
+		return query.getResultList();
+	}
 
 }
--- e-override-ejb/src/main/java/gestionCommandservices/interfaces/GestionCommandserviceRemote.java
+++ e-override-ejb/src/main/java/gestionCommandservices/interfaces/GestionCommandserviceRemote.java
@@ -1,8 +1,19 @@
 package gestionCommandservices.interfaces;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import entities.Command;
+import entities.Product;
 
 @Remote
 public interface GestionCommandserviceRemote {
-
+	List<Command> findAllCommandsByClient(String ClientName);
+	List<Command> findAllCommandsByCategory(String ProductCategory);
+	List<Command> findAllCommandsByCommandDate(Date CommandDate);
+	List<Command> findAllCommandsByDeliveryDate(Date DeliveryDate);
+	List<Product> findAllCommandsByClientName(String ClientName);
+	List<Product> findAllProductsByIdCommand(Integer CommandId);
 }
