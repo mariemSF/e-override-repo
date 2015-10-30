401dbfc1da2c53ac9e66a528fb520bfa39e09e3e
mariem <mariem@172.16.67.75>
2015-10-30 16:53:52 +0100

    Signed-off-by: mariem <mariem@172.16.67.75>

--- e-override-ejb/src/main/java/gestionCrudservices/impl/GestionCrudservice.java
+++ e-override-ejb/src/main/java/gestionCrudservices/impl/GestionCrudservice.java
@@ -9,6 +9,9 @@ import gestionCrudservices.interfaces.GestionCrudserviceLocal;
 import gestionCrudservices.interfaces.GestionCrudserviceRemote;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  * Session Bean implementation class GestionCrudservice
@@ -19,44 +22,96 @@ public class GestionCrudservice implements GestionCrudserviceRemote, GestionCrud
     /**
      * Default constructor. 
      */
+	@PersistenceContext
+	private EntityManager entityManager;
+	
     public GestionCrudservice() {
         // TODO Auto-generated constructor stub
     }
 
 	@Override
-	public Client loginClient(String name, String password) {
-		// TODO Auto-generated method stub
-		return null;
+	public Client loginClient(String login, String password) {
+		String jpql = "select c from Client c where c.login=:param1 and c.password=:param2";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param1", login);
+		query.setParameter("param2", password);
+		Client client = null;
+		try {
+			client = (Client) query.getSingleResult();
+		} catch (Exception e) {
+			System.err.println("bad credentials");
+		}
+		return client;
 	}
 
 	@Override
-	public Provider loginProvider(String name, String password) {
-		// TODO Auto-generated method stub
-		return null;
+	public Provider loginProvider(String login, String password) {
+		String jpql = "select p from Provider p where p.login=:param1 and p.password=:param2";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param1", login);
+		query.setParameter("param2", password);
+		Provider provider = null;
+		try {
+			provider = (Provider) query.getSingleResult();
+		} catch (Exception e) {
+			System.err.println("bad credentials");
+		}
+		return provider;
 	}
 
 	@Override
 	public Boolean addProduct(Product produit) {
-		// TODO Auto-generated method stub
-		return null;
+		Boolean b = false;
+		try {
+			entityManager.persist(produit);
+			b = true;
+		} catch (Exception e) {
+		}
+		return b;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Product> findAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		String jpql = "select p from Product p";
+		Query query = entityManager.createQuery(jpql);
+		return query.getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Provider> findAllProviders() {
-		// TODO Auto-generated method stub
-		return null;
+		String jpql = "select p from Provider p";
+		Query query = entityManager.createQuery(jpql);
+		return query.getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Client> findAllClients() {
-		// TODO Auto-generated method stub
-		return null;
+		String jpql = "select p from Provider p";
+		Query query = entityManager.createQuery(jpql);
+		return query.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Product> findAllProductsByCategory(String pCategory) {
+		String jpql = "select p from Product p where p.category=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", pCategory);
+		return query.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Product> findAllProductsByProviderName(String pName) {
+		Provider provider = entityManager.find(Provider.class,
+				pName);
+		String jpql = "select p from Product p where p.provider=:param";
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("param", provider);
+		return query.getResultList();
 	}
 
 }
