e60dc27f5dc6a8f74377e4d802722dde522c77b0
mohamed_klibi23 <mohamed_klibi23@HP-KLIBI>
2015-11-03 17:07:18 +0100

    Signed-off-by: mohamed_klibi23 <mohamed_klibi23@HP-KLIBI>

---
+++ e-override-client/src/main/java/tests/orderManagementServicesTests/TestFindAllOrdersByCategory.java
@@ -0,0 +1,10 @@
+package tests.orderManagementServicesTests;
+
+public class TestFindAllOrdersByCategory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
